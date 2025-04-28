044229162d937df049da79d5c3e921e6f8b8188e
Maniha-dev <nasirrazaptcl@gmail.com>
2025-04-28 00:49:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let radius=prompt("Enter Radius")
+let cir=2*3.14*radius
+let dis=("Circumferance of circle is " + cir)
+console.log(dis)
+let dia=2*radius
+let dis1=("Diameter of circle is " + dia)
+console.log(dis1)
+let area=3.14*radius**2
+let pro=(alert("Area of Circle is " + area))
+let dis3=("Area of circle is " + area)
+console.log(dis3)
